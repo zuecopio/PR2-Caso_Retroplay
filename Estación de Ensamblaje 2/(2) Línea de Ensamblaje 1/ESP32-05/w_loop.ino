402eecb70e41808ef2a36c06b644d4f42f9874cd
Angy <145704908+AngyOwO@users.noreply.github.com>
2024-04-18 16:12:12 +0200

    Update w_loop.ino
    Modified infoIn("Estación 2, Línea 1...");

--- Estación de Ensamblaje 2/(2) Línea de Ensamblaje 1/ESP32-05/w_loop.ino
+++ Estación de Ensamblaje 2/(2) Línea de Ensamblaje 1/ESP32-05/w_loop.ino
@@ -12,10 +12,14 @@
  * @section  PR2-GIIROB
  */
 
+#if !(US_ONLY_WHEN_LINE_IS_FREE) // When US_ONLY_WHEN_LINE_IS_FREE = 0, that #if is working.
+
 long now, lastMsg = 0;
 
 // Tiempo de actualización de los sensores.
 long sensorsUpdateInterval = 5000;
+
+#endif
 
 /******************************************************************************/
 /*!
@@ -25,16 +29,56 @@ long sensorsUpdateInterval = 5000;
  */
 void on_loop(void)
 {
+    if (emergency_button.pressed)
+    {
+        // Create a JSON document.
+        JsonDocument doc;
+        doc["emergencia"] = "si";
+
+        // Serialize the JSON to a String.
+        String msg_json;
+        serializeJson(doc, msg_json);
+
+        // Send message by a topic.
+        enviarMensajePorTopic(EMERGENCY_BUTTON_STATUS_TOPIC, msg_json);
+        
+        infoln("Estación 2, Línea 1: ¡El botón de emergencia se ha pulsado!");
+        emergency_button.pressed = false;
+    }
+
+    #if !(US_ONLY_WHEN_LINE_IS_FREE) // When US_ONLY_WHEN_LINE_IS_FREE = 0, that #if is working.
+    
     now = millis();
   
     if ((now - lastMsg) > sensorsUpdateInterval)
     {
         lastMsg = now;
     
+        // Get the distance from the sensor ...
         //
-        // Read and process sensors ...
+        distance = getUsDistance();
+
+        // If the distance is less than 10 cm, there is
+        // a new P-C placed at the entrance of the line.
         //
+        if (distance < 10.0)
+        {
+            // Create a JSON document.
+            JsonDocument doc;
+            doc["entrada"] = "hay";
+    
+            // Serialize the JSON to a String.
+            String msg_json;
+            serializeJson(doc, msg_json);
+    
+            // Send message by a topic.
+            enviarMensajePorTopic(LINE_ENTRANCE_STATUS_TOPIC, msg_json);
+            
+            infoln("Estación 2, Línea 1: Hay un P-C disponible en la entrada");
+        }
     }
+
+    #endif
 
 }   /* on_loop() */
 
