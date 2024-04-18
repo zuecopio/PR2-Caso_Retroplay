2101d1c9355e633b42ac22a9229b25da8d00b80b
Angy <145704908+AngyOwO@users.noreply.github.com>
2024-04-18 15:59:01 +0200

    Update f_funciones.ino

--- Estación de Ensamblaje 2/(2) Línea de Ensamblaje 1/ESP32-05/f_funciones.ino
+++ Estación de Ensamblaje 2/(2) Línea de Ensamblaje 1/ESP32-05/f_funciones.ino
@@ -18,12 +18,25 @@
  * @param  void
  * @return void
  */
-void example(void)
+float getUsDistance(void)
 {
-    // TODO: Desarrollo de la función ...
-    //
-    // TODO: Deberíamos publicar el estado del dispositivo cada vez que cambie.
-
-}   /* example() */
+    unsigned long pingTime;
+    float distance;
+    
+    // Make trigPin output high level lasting for 10μs to triger HC_SR04.
+    digitalWrite(trigPin, HIGH); 
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    
+    // Wait HC-SR04 returning to the high level and measure out this waitting time.
+    pingTime = pulseIn(echoPin, HIGH, timeOut); 
+    
+    // Calculate the distance according to the time.
+    distance = (float)pingTime * soundVelocity / 2 / 10000;
+    
+    // Return the distance value.
+    return distance;
+    
+}   /* getUsDistance() */
 
 /*** end of file ***/
